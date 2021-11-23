58b1735f00e44e46039a963c972c6c2b568817ec
Eddod <edwinwesterberg44@gmail.com>
2021-11-23 10:33:35 +0100

    Add project files.

---
+++ GenericCompare/Check.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCompare
+{
+    public class Check<Unknown>
+    {
+        public bool Compare(Unknown t1, Unknown t2)
+        {
+            if (t1.Equals(t2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class CheckNumbers : Check<int>
+    {
+        
+            
+    }
+
+    public class CheckString : Check<string>
+    { 
+        
+    
+    }
+
+}
---
+++ GenericCompare/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckNumbers n1 = new CheckNumbers();            
+            CheckNumbers n2 = new CheckNumbers();            
+            CheckString s1 = new CheckString();            
+            CheckString s2 = new CheckString();
+            
+
+            Console.WriteLine(n1.Compare(1, 2));
+            Console.WriteLine(n2.Compare(-2,-2));
+            Console.WriteLine(s1.Compare("Hej", "Hello"));
+            Console.WriteLine(s2.Compare("Hello", "Hello"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
